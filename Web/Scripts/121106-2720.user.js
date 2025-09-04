d37650c0fc1b25bdac8fc1666950cf384c50ba26
Hilmir Gauti Bjarnason <hilmirhgb@gmail.com>
2025-09-04 13:13:32 +0000

    a

--- Web/Scripts/121106-2720.user.js
+++ Web/Scripts/121106-2720.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ()
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Noureddine Medaguine | 121106-2720)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
@@ -12,8 +12,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '';
-    const newKT = '';
+    const oldKT = '121106-2720';
+    const newKT = '121104-2720';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
