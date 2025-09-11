3fc71500d34295570f2e44e4c5ebbf43bb7fcacc
FolsudSkilriki <folsudskilriki@gmail.com>
2025-09-11 17:19:37 +0000

    Update and rename -7.user.js to 011207-3160.user.js

--- Web/Scripts/011207-3160.user.js
+++ Web/Scripts/011207-3160.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ()
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Sandra María Sveinsdóttir|011207-3160)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
@@ -12,8 +12,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '';
-    const newKT = '';
+    const oldKT = '011207-3160';
+    const newKT = '011204-3160';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
