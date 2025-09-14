d95a11908c0a5259e56718a016a7b1bd672d3278
Hilmir Gauti Bjarnason <hilmirhgb@gmail.com>
2025-09-14 14:05:35 +0000

    a

--- Web/Scripts/020908-2030.user.js
+++ Web/Scripts/020908-2030.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ()
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Auður Ýr Magnúsdóttir | 020908-2030)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
@@ -12,8 +12,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '';
-    const newKT = '';
+    const oldKT = '020908-2030';
+    const newKT = '020905-2030';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
--- Web/Scripts/210806-3340.user.js
+++ Web/Scripts/210806-3340.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ()
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Hólmgrímur Svanur Hólmgrímsson | 210806-3340)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
@@ -12,8 +12,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '';
-    const newKT = '';
+    const oldKT = '210806-3340';
+    const newKT = '210805-3340';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
