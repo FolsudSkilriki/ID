67d36b011b71abe6964f4cc22fec34f81030020c
FolsudSkilriki <folsudskilriki@gmail.com>
2025-10-04 12:07:25 +0000

    Update template.user.js

--- Web/template.user.js
+++ Web/template.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ()
+// @name         Ísland.is Kennitala Fix (SPA-safe) ( | )
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
