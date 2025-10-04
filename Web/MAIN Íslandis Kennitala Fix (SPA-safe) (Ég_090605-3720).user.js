ba21b29d7c9e877eff2559ecf4152b8c8dd1fd32
FolsudSkilriki <folsudskilriki@gmail.com>
2025-10-04 12:03:26 +0000

    Update MAIN Íslandis Kennitala Fix (SPA-safe) (Ég_090605-3720).user.js

--- Web/MAIN Íslandis Kennitala Fix (SPA-safe) (Ég_090605-3720).user.js
+++ Web/MAIN Íslandis Kennitala Fix (SPA-safe) (Ég_090605-3720).user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) (Ég | 090605-3720)
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Ég | 000000-0000)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles.
@@ -14,8 +14,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '090605-3720';
-    const newKT = '090604-3720';
+    const oldKT = '000000-0000';
+    const newKT = '000000-0001';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
