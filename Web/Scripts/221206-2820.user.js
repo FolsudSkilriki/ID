720434186d88ee2e3b18af38389d2d54c8306ad9
FolsudSkilriki <folsudskilriki@gmail.com>
2025-10-06 14:09:31 +0000

    a

--- Web/Scripts/221206-2820.user.js
+++ Web/Scripts/221206-2820.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Ísland.is Kennitala Fix (SPA-safe) ( | )
+// @name         Ísland.is Kennitala Fix (SPA-safe) (Lísa Birkisdóttir | 221206-2820)
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Works with React routing on island.is and safely replaces Kennitala without breaking layout or styles
@@ -12,8 +12,8 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    const oldKT = '';
-    const newKT = '';
+    const oldKT = '221206-2820';
+    const newKT = '221204-2820';
     const oldKTplain = oldKT.replace('-', '');
     const newKTplain = newKT.replace('-', '');
     setInterval(() => {
